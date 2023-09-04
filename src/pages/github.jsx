72c823a19831d8505660f23cfad03907580b86c8
arjundangi01 <arjundangi907473@gmail.com>
2023-09-04 10:21:46 +0530

    added1

--- src/pages/github.jsx
+++ src/pages/github.jsx
@@ -8,21 +8,21 @@ const Github = () => {
           <img src="https://github-profile-trophy.vercel.app/?username=arjundangi01" alt="" />
         </div>
         <div className={  `${style.top}`} >
-          <div  id="github-top-langs" >
-            <img src="http://github-profile-summary-cards.vercel.app/api/cards/repos-per-language?username=arjundangi01&theme=2077" alt="" />
+          <div   >
+            <img id="github-top-langs" src="http://github-profile-summary-cards.vercel.app/api/cards/repos-per-language?username=arjundangi01&theme=2077" alt="" />
           </div>
           <div>
             <img src="http://github-profile-summary-cards.vercel.app/api/cards/most-commit-language?username=arjundangi01&theme=2077" alt="" />
           </div>
         </div>
-        <div id="github-stats-card" >
-          <img src="http://github-profile-summary-cards.vercel.app/api/cards/profile-details?username=arjundangi01&theme=2077" alt="" />
+        <div  >
+          <img id="github-stats-card" src="http://github-profile-summary-cards.vercel.app/api/cards/profile-details?username=arjundangi01&theme=2077" alt="" />
         </div>
-        <div id="github-streak-stats" >
-          <img src="https://github-readme-streak-stats.herokuapp.com/?user=arjundangi01&theme=tokyonight_duo&hide_border=true&background=DD272700" alt="" />
+        <div >
+          <img  id="github-streak-stats" src="https://github-readme-streak-stats.herokuapp.com/?user=arjundangi01&theme=tokyonight_duo&hide_border=true&background=DD272700" alt="" />
         </div>
         <div>
-          <img src="http://ghchart.rshah.org/shivraj32644" alt="" />
+          <img class="react-activity-calendar" src="http://ghchart.rshah.org/arjundangi01" alt="" />
         </div>
       </div>
     </div>
