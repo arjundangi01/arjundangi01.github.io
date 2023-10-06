bac9f181750a33d06abbacc42de8551f8391b1d8
arjundangi01 <arjundangi907473@gmail.com>
2023-10-06 19:49:45 +0530

    d4

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,13 +6,13 @@ export default function Navbar() {
     <nav className="navbar navbar-expand-lg navSec  ">
       <div
         id="nav-menu"
-        className={`container-fluid fixed-top pe-lg-5 pe-md-5 ps-5 pt-1 pb-1   ${style.nav_menu}`}
+        className={`container-fluid fixed-top pe-xl-5 pe-lg-2 ps-lg-2 pe-md-2 ps-xl-5 ps-md-2 pt-1 pb-1   ${style.nav_menu}`}
       >
-        <a className="navbar-brand d-flex  align-items-center " href="#">
+        <a className="navbar-brand d-flex ms-lg-5 ms-md-0  align-items-center  " href="#">
           <span className={style.span}>A</span>rjun{" "}
           <span>
             <img
-              className="ms-lg-2"
+              className="ms-lg-2 ms-md-2"
               src="https://camo.githubusercontent.com/beb64ff21c883e318e4f5db5231c2ba4175705bea1c9249e82a41ab375db4f75/68747470733a2f2f6d65646961322e67697068792e636f6d2f6d656469612f51737347456d706b79454f684243623765312f67697068792e6769663f6369643d656366303565343761306e336769316266716e74716d6f62386739616964316f796a327772336473336d67373030626c267269643d67697068792e676966"
               width="35"
             />
@@ -35,42 +35,42 @@ export default function Navbar() {
         >
           <div className="navbar-nav align-items-center ">
             <a
-              className="nav-link home active me-5 text-info-emphasis "
+              className="nav-link home active me-lg-2 me-xl-5 me-md-5 text-info-emphasis "
               aria-current="page"
               href="#home"
             >
               HOME
             </a>
             <a
-              className="nav-link about active me-5 text-info-emphasis "
+              className="nav-link about active me-lg-2 me-xl-5 me-md-5 text-info-emphasis "
               aria-current="page"
               href="#about"
             >
               ABOUT
             </a>
             <a
-              className="nav-link skills active me-5 text-info-emphasis "
+              className="nav-link skills active me-lg-2 me-xl-5 me-md-5 text-info-emphasis "
               aria-current="page"
               href="#skills"
             >
               SKILLS
             </a>
             <a
-              className="nav-link projects active me-5 text-info-emphasis"
+              className="nav-link projects active me-lg-2 me-xl-5 me-md-5 text-info-emphasis"
               aria-current="page"
               href="#projects"
             >
               PROJECTS
             </a>
             <a
-              className="nav-link active me-5 text-info-emphasis"
+              className="nav-link active me-lg-2 me-xl-5 me-md-5 text-info-emphasis"
               aria-current="page"
               href="#github"
             >
               GITHUB
             </a>
             <a
-              className="nav-link contact active me-5 text-info-emphasis"
+              className="nav-link contact active me-lg-2 me-xl-5 me-md-5 text-info-emphasis"
               aria-current="page"
               href="#contact"
             >
