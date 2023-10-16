8c281613edde494451b907116d1df7bb90c4e601
arjundangi01 <arjundangi907473@gmail.com>
2023-10-16 14:11:49 +0530

    changes1

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,10 +3,10 @@ import style from "../styles/navbar.module.css";
 import pdf from "../pages/Images/Arjun_Dangi_Resume.pdf";
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navSec  ">
+    <nav className="navbar navbar-expand-lg navSec ps-xl-0 ">
       <div
         id="nav-menu"
-        className={`container-fluid fixed-top pe-xl-5 pe-lg-2 ps-lg-2 pe-md-2 ps-xl-5 ps-md-2 pt-1 pb-1   ${style.nav_menu}`}
+        className={`container-fluid fixed-top pe-2 ps-2 pe-sm-5 ps-sm-5 ps-md-5  ps-lg-0 ps-xl-5   pe-md-5  pe-lg-0 pe-xl-5 pt-1 pb-1 ${style.nav_menu}`}
       >
         <a className="navbar-brand d-flex ms-lg-5 ms-md-0  align-items-center  " href="#">
           <span className={style.span}>A</span>rjun{" "}
@@ -35,49 +35,49 @@ export default function Navbar() {
         >
           <div className="navbar-nav align-items-center ">
             <a
-              className="nav-link home active me-lg-2 me-xl-5 me-md-5 text-info-emphasis "
+              className="nav-link home active me-lg-2 me-xl-5  text-info-emphasis "
               aria-current="page"
               href="#home"
             >
               HOME
             </a>
             <a
-              className="nav-link about active me-lg-2 me-xl-5 me-md-5 text-info-emphasis "
+              className="nav-link about active me-lg-2 me-xl-5  text-info-emphasis "
               aria-current="page"
               href="#about"
             >
               ABOUT
             </a>
             <a
-              className="nav-link skills active me-lg-2 me-xl-5 me-md-5 text-info-emphasis "
+              className="nav-link skills active me-lg-2 me-xl-5 text-info-emphasis "
               aria-current="page"
               href="#skills"
             >
               SKILLS
             </a>
             <a
-              className="nav-link projects active me-lg-2 me-xl-5 me-md-5 text-info-emphasis"
+              className="nav-link projects active me-lg-2 me-xl-5  text-info-emphasis"
               aria-current="page"
               href="#projects"
             >
               PROJECTS
             </a>
             <a
-              className="nav-link active me-lg-2 me-xl-5 me-md-5 text-info-emphasis"
+              className="nav-link active me-lg-2 me-xl-5 text-info-emphasis"
               aria-current="page"
               href="#github"
             >
               GITHUB
             </a>
             <a
-              className="nav-link contact active me-lg-2 me-xl-5 me-md-5 text-info-emphasis"
+              className="nav-link contact active me-lg-2 me-xl-5  text-info-emphasis"
               aria-current="page"
               href="#contact"
             >
               CONTACT
             </a>
             <a
-              className="nav-link resume active me-5 text-info-emphasis"
+              className="nav-link resume active   me-5 text-info-emphasis"
               id="resume-link-1"
               aria-current="page"
               onClick={() =>
